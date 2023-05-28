f9bf21851c9d3060f2339b857c11220352811d83
jesussantosupf <jesus.santos01@estudiant.upf.edu>
2023-05-28 12:45:01 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/CodigoDePruebaGit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodigoDePruebaGit.dir/main.c.obj"
+  "CodigoDePruebaGit.exe"
+  "CodigoDePruebaGit.exe.manifest"
+  "CodigoDePruebaGit.pdb"
+  "libCodigoDePruebaGit.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CodigoDePruebaGit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
